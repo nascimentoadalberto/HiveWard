f51064a408f4825c5d2e7dd7005a8abae7c5c59e
Guilherme Duarte Brandão Simões Silva <GUILHERME.SSILVA@bandtec.com.br>
2021-09-02 10:07:34 -0300

    script gui-henrique

---
+++ Banco de Dados/esboco das empresas gui-henrique.sql
@@ -0,0 +1,10 @@
+create database Grandes;
+use Grandes;
+create table Porte1(
+id int primary key,
+nome varchar (70),
+cep varchar(70),
+ numeração_de_sensor varchar (70),
+numeração_das_comeias varchar (70)
+);
+select * from Porte;
